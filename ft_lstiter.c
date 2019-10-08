11e01958598f6254f0c6ef3fb8cc351e9c57d86e
Ilya Klimov <iklimov@e1z2r4p15.42.us.org>
2019-10-07 21:18:02 -0700

    ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,17 +6,17 @@
 /*   By: iklimov <iklimov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/29 21:43:23 by iklimov           #+#    #+#             */
-/*   Updated: 2019/10/02 20:43:57 by iklimov          ###   ########.fr       */
+/*   Updated: 2019/10/07 21:11:51 by iklimov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-    if ((*lst).next)
-        ft_lstiter((*lst).next, f);
-	f((*lst).content);
-	free(lst);
-	lst = NULL;
+	while (lst)
+	{
+		f(lst);
+		lst = (*lst).next;
+	}
 }
